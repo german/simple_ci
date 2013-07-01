e15d267bfafc151863c0053e6b0f096412c66da7
Dmytro Minin <dmytro.minin@gmail.com>
2013-07-01 17:08:57 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCi::Application.config.secret_key_base = '86740eb062c316c165968a4483f6f82247eafc06b5a33406912d0cac698a69bb34d4bf4c72c3d509ce84f82e0f4b361fe65075f01f4f38a758e591b93be0d2b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleCi::Application.config.session_store :cookie_store, key: '_simple_ci_session'
