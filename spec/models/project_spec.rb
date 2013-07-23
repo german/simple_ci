42132f721eaa263c20fac02559deae1fee17c4f6
Dmitrii Samoilov <e2718281828@ya.ru>
2013-07-23 10:01:54 +0300

    added rspec files and deleted test/ folder

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Project do
+  it "should have unique name and path on filesystem"
+  
+  it "should have full qualified path"
+  
+  it "should have rspec/cucumber/test::unit option selected"
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe User do
+  let(:subject) {User.new}
+  
+  it "should be able to create new project"
+  
+  it "should be able to delete existing project (created by himself)"
+  
+  it "should be able to change name of existing project"
+  
+  it "should be able to change path to existing project"
+end
