2c5aa8ce5c173f340e40e2cb9f23bdc482a20678
Dmitrii Samoilov <e2718281828@ya.ru>
2013-07-31 16:34:07 +0300

    moved rails_admin from /admin to /

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 SimpleCi::Application.routes.draw do
   devise_for :users
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  mount RailsAdmin::Engine => '/', :as => 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => 'home#index'
+  #root :to => 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
