94ef60a0fa332fdeab30ad77c1af3ed2bf3fd53b
Dmitrii Samoilov <e2718281828@ya.ru>
2013-08-16 15:01:38 +0300

    deleted unused migration

--- db/migrate/20130814134409_add_new_fields_to_projects.rb
+++
@@ -1,8 +0,0 @@
-class AddNewFieldsToProjects < ActiveRecord::Migration
-  def change
-    add_column :projects, :output, :text
-    add_column :projects, :failed_tests, :integer
-    add_column :projects, :pending_tests, :integer
-    add_column :projects, :succeeded_tests, :integer
-  end
-end
