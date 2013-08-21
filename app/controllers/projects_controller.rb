84c141e1ac3d4f7ca6f19e6d291d419ada212308
Dmitrii Samoilov <e2718281828@ya.ru>
2013-08-21 23:52:18 +0300

    added preferences page for the project (w/ update attributes functionality)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,17 +5,28 @@ class ProjectsController < ApplicationController
   def index
     @projects = current_user.projects
   end
-  
+
+  def update
+    @project = current_user.projects.find_by!(id: params[:id])
+    @project.update_attributes!(project_params)
+    redirect_to projects_path #@project
+  end
+    
   def enqueue
-    @project = Project.find_by!(id: params[:id])
+    @project = current_user.projects.find_by!(id: params[:id])
     @project.enqueue!
     respond_with(@project)
   rescue AASM::InvalidTransition => e
     @project.fail!
     render nothing: true
   end
-  
+
   def preferences
     @project = Project.find_by!(id: params[:id])
+  end
+  
+private
+  def project_params
+    params.require(:project).permit(:name, :path_to_rails_root)
   end
 end
