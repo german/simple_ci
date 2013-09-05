5381696017c1befd89d262987f543f1464c4bf3b
Dmitrii Samoilov <e2718281828@ya.ru>
2013-09-05 17:01:11 +0300

    fixed typo in project_status_controller

--- app/controllers/project_status_controller.rb
+++ app/controllers/project_status_controller.rb
@@ -1,6 +1,6 @@
 class ProjectStatusController < ActionController::Metal
   def check
     project = Project.find params[:id]
-    self.response_body = "{status: #{project.aasm_state}}"
+    self.response_body = "{status: #{project.state}}"
   end
 end
